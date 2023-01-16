1b71b761e4f91d88516250ba293a9927ac9ab2b4
salahelfatimi <salahfatimi16@gmail.com>
2023-01-16 01:09:01 +0100

    Update Nav.jsx

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -24,7 +24,7 @@ export default function Nav() {
       <Disclosure as="nav" className="bg-gray-800">
         {({ open }) => (
           <>
-            <div className="mx-auto container px-4 sm:px-6 lg:px-8">
+            <div className="mx-auto  px-4 sm:px-6 lg:px-8">
               <div className="flex h-16 items-center justify-between">
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
