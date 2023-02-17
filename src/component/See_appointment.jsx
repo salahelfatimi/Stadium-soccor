bce97204f5f89c3b7788bc40f5ba0c8f067eaf69
ELFATIMI SALAHEDDINE <salahfatimi16@gmail.com>
2023-02-17 23:24:00 +0100

    init2

--- src/component/See_appointment.jsx
+++ src/component/See_appointment.jsx
@@ -4,7 +4,7 @@ export default () => {
   return (
     <div className="flex flex-col justify-around p-6">
       <div className="grid grid-cols-1  gap-x-4 items-center ">
-        <div className="text-[#FEFAF1]  bg-[#1e1f23] p-7 rounded-3xl  ">
+        <div className="text-[#FEFAF1]  bg-[#1e1f23] p-6 rounded-3xl  ">
           <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10">
             
               <img src={playground} alt="" />
